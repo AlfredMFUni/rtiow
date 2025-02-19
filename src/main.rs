3ff0e7366b789990532e4b8c16619cf46de93f72
Mason-fayle <b9014858@hallam.shu.ac.uk>
2025-02-19 14:13:49 +0000

    Made texture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    //Create an image
+    let image_height = 256;
+    let image_width = 256;
+
+    let mut image_buffer: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> = image::ImageBuffer::new(image_width, image_height);
+
+    for (x,y,pixel) in image_buffer.enumerate_pixels_mut()
+    {
+        let r: f64 = (x as f64) / (image_width as f64 - 1.0);
+        let g: f64 = (y as f64) / (image_height as f64 - 1.0);
+        let b: f64 = 0.0;
+
+        *pixel = image::Rgb([(r * 255.999) as u8, (g * 255.999) as u8, (b * 255.999) as u8]);
+    }
+
+    image_buffer.save("image.png").unwrap();
+    // Render
 }
