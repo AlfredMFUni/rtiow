69207b39e4ec79bf9e71130da8d988a86fb49e26
Mason-fayle <b9014858@hallam.shu.ac.uk>
2025-02-21 15:28:20 +0000

    refactored into lib file

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use image::ImageBuffer;
+
+pub fn render(img_buf: &mut ImageBuffer<image::Rgb<u8>, Vec<u8>>){
+    let width: u32 = img_buf.width();
+    let height: u32 = img_buf.height();
+    for (x,y,pixel) in img_buf.enumerate_pixels_mut()
+    {
+        let r: f64 = (x as f64) / (width as f64 - 1.0);
+        let g: f64 = (y as f64) / (height as f64 - 1.0);
+        let b: f64 = 0.0;
+
+        *pixel = image::Rgb([(r * 255.999) as u8, (g * 255.999) as u8, (b * 255.999) as u8]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-
-use image::ImageBuffer;
-
-fn render(img_buf: &mut ImageBuffer<image::Rgb<u8>, Vec<u8>>){
-    let width: u32 = img_buf.width();
-    let height: u32 = img_buf.height();
-    for (x,y,pixel) in img_buf.enumerate_pixels_mut()
-    {
-        let r: f64 = (x as f64) / (width as f64 - 1.0);
-        let g: f64 = (y as f64) / (height as f64 - 1.0);
-        let b: f64 = 0.0;
-
-        *pixel = image::Rgb([(r * 255.999) as u8, (g * 255.999) as u8, (b * 255.999) as u8]);
-    }
-}
-
 fn main() {
     //Create an image
     let image_height = 256;
@@ -21,7 +5,7 @@ fn main() {
 
     let mut image_buffer: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> = image::ImageBuffer::new(image_width, image_height);
 
-    render(&mut image_buffer);
+    rtiow::render(&mut image_buffer);
 
     image_buffer.save("image.png").unwrap();
     // Render
