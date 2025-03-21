82f20c148488af4074aa1069906e7ca1863b8e08
Mason-fayle <b9014858@hallam.shu.ac.uk>
2025-03-21 15:10:21 +0000

    Added sphere

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,39 @@
 pub mod vec3; //includes the sub-module color 
 pub mod ray;
 
+use std::mem::Discriminant;
+
 use image::{ImageBuffer, Rgb};
 use vec3::color::Color; 
 use vec3::Vec3;
 use ray::Ray;
 
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> f64 {
+  let oc = *center - *r.origin();
+  let a = Vec3::dot(r.direction(), r.direction());
+  let b = 2.0 * Vec3::dot(r.direction(), &oc);
+  let c = Vec3::dot(&oc, &oc) - radius * radius;
+  //The quadratic equation a*t*t + b*t + c = 0 has real roots 
+  // iff (-b +\- sqrt(b*b - 4*a*c)) / 2*a has a solution
+  // iff b*b - 4*a*c >= 0 
+  let discriminant = b * b - 4.0 * a * c;
+
+  if discriminant < 0.0 { -1.0 } else { ( b -discriminant.sqrt() ) / (2.0 * a) }
+}
+
 fn ray_color(r: &Ray) -> Color {
-  let unit_direction= Vec3::unit_vector(r.direction());
-  let a = 0.5 * (unit_direction.y + 1.0); 
-  (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
+  let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+  if t > 0.0 {
+    //Surface normal direction is from center of sphere to hit point
+    let surface_normal = Vec3::unit_vector(&(r.point_at(t) - Vec3::new(0.0, 0.0, -1.0)));
+    //surface_normal has -1 <= x, y, z <= 1 so to get a colour just map
+    //  these values into [0 .. 1] by value -> (value + 1) / 2
+    0.5 * Color::new(surface_normal.x + 1.0, surface_normal.y + 1.0, surface_normal.z + 1.0)
+  } else {
+    let unit_direction = Vec3::unit_vector(r.direction());
+    let a = 0.5 * (unit_direction.y + 1.0); 
+    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
+  }
 }
 
 ///Render an image onto the supplied ImageBuffer  
