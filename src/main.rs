aaebf224e11c6f0d98a615d30d43269dcf222c54
Mason-fayle <b9014858@hallam.shu.ac.uk>
2025-04-09 14:11:53 +0100

    Hollow Glass

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,8 @@ fn main() {
   // would incorrectly infer a type of Rc<Lambertian>, Rc<Metal>, etc. 
   let material_ground: Rc<dyn Material>  = Rc::new(Lambertian{albedo: Color::new(0.8, 0.8, 0.0)});
   let material_center: Rc<dyn Material> = Rc::new(Lambertian{albedo: Color::new(0.1, 0.2, 0.5)});
-  let material_left: Rc<dyn Material> = Rc::new(Dielectric{refraction_index: 1.0 / 1.33});
+    let material_left: Rc<dyn Material> = Rc::new(Dielectric{refraction_index: 1.5});         //Refactor
+  let material_bubble: Rc<dyn Material> = Rc::new(Dielectric{refraction_index: 1.0 / 1.5}); //Add
   let material_right: Rc<dyn Material> = Rc::new(Metal{albedo: Color::new(0.8, 0.6, 0.2), fuzz: 0.1});
 
   //Create the World: we must place hittable objects into the scene  
@@ -37,10 +38,11 @@ fn main() {
   world.add(Rc::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, material_ground.clone())));
   world.add(Rc::new(Sphere::new(Vec3::new(0.0, 0.0, -1.2), 0.5, material_center.clone())));
   world.add(Rc::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, material_left.clone())));
+  world.add(Rc::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.4, material_bubble.clone())));
   world.add(Rc::new(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, material_right.clone())));
   
   // eprint!("Starting render\n");
-  rtiow::render(&mut image_buffer, &world, 50, 5); 
+  rtiow::render(&mut image_buffer, &world, 50, 10); 
   
   // Write the ImageBuffer to a file
   //  We can ignore errors for now so just "unwrap" the Ok result.
